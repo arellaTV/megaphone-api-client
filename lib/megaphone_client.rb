a8447a5fe87e4a51d350eec7cedccd716aed660c
arellaTV <jeremiah.arella@gmail.com>
2018-04-02 10:49:34 -0700

    Adds inline styles with yard doc

--- lib/megaphone_client.rb
+++ lib/megaphone_client.rb
@@ -5,10 +5,15 @@ module MegaphoneClient
   class ConnectionError < StandardError
   end
 
+  # @author Jay Arella
   class << self
     attr_accessor :api_base_url, :network_id, :organization_id, :token
 
-    def new(options)
+    # @option options [String] :api_base_url The api base url that includes the domain and /api prefix
+    # @option options [String] :network_id The id of the Megaphone network you'd like to interface with
+    # @option options [String] :organization_id The id of the Megaphone organization you'd like to interface with
+    # @option options [String] :token The api token taken from Megaphone's developer settings (https://developers.megaphone.fm/)
+    def new options={}
       @api_base_url = options[:api_base_url] || "https://cms.megaphone.fm/api"
       @network_id = options[:network_id]
       @organization_id = options[:organization_id]
