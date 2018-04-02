f7864aa0a2205a582f4b84d8733e3568f69bc8af
arellaTV <jeremiah.arella@gmail.com>
2018-04-02 13:15:07 -0700

    Adds inline documentation on the module level

--- lib/megaphone_client.rb
+++ lib/megaphone_client.rb
@@ -9,10 +9,15 @@ module MegaphoneClient
   class << self
     attr_accessor :api_base_url, :network_id, :organization_id, :token
 
-    # @option options [String] :api_base_url The api base url that includes the domain and /api prefix
-    # @option options [String] :network_id The id of the Megaphone network you'd like to interface with
-    # @option options [String] :organization_id The id of the Megaphone organization you'd like to interface with
+    # @option options [String] :api_base_url The api base url includes the domain and `/api` endpoint
+    # @option options [String] :network_id The id of the Megaphone network
+    # @option options [String] :organization_id The id of the Megaphone organization
     # @option options [String] :token The api token taken from Megaphone's developer settings (https://developers.megaphone.fm/)
+    # @return a new instance of MegaphoneClient
+    # @example Initialize MegaphoneClient into an instance
+    #   @megaphone = MegaphoneClient.new({ network_id: '1234', organization_id: '5678', token: '910' })
+    #   @megaphone #=> new MegaphoneClient
+
     def new options={}
       @api_base_url = options[:api_base_url] || "https://cms.megaphone.fm/api"
       @network_id = options[:network_id]
@@ -21,6 +26,15 @@ module MegaphoneClient
 
       self
     end
+
+    # @option options [String] :method  Request method
+    # @option options [Hash] :params Request params in a hash
+    # @option options [String] :payload If the request method is `POST`, the body of the post
+    # @option options [String] :url Request url
+    # @note This is a generalized REST method that is used by both the Episode and Podcast class.
+    #   If it's successful, it returns a struct representing the data. If it fails, it raises a ConnectionError.
+    # @example Get a list of podcasts
+    #   @megaphone.connection({ method: "GET", url: "https://cms.megaphone.fm/api/podcasts" }) #=> Array of structs representing podcasts
 
     def connection options={}
       request_headers = default_headers.merge({ params: options[:params] })
@@ -39,6 +53,11 @@ module MegaphoneClient
       JSON.parse(response.body, object_class: OpenStruct)
     end
 
+    # @return a hash with the default request headers, includes the token
+    # @example Calling #default_headers after initializing with a token
+    #   @megaphone = MegaphoneClient.new({ network_id: '1234', organization_id: '5678', token: '910' })
+    #   @megaphone.default_headers #=> { content_type: "application/json", authorization: "Token token=910", params: {} }
+
     def default_headers
       {
         content_type: "application/json",
@@ -47,9 +66,19 @@ module MegaphoneClient
       }
     end
 
+    # @return a new instance of MegaphoneClient::Episode
+    # @example Initialize MegaphoneClient into an instance
+    #   @megaphone = MegaphoneClient.new({ network_id: '1234', organization_id: '5678', token: '910' })
+    #   @megaphone.episode #=> new MegaphoneClient::Episode
+
     def episode
       self::Episode
     end
+
+    # @return a new instance of MegaphoneClient::Podcast
+    # @example Initialize MegaphoneClient into an instance
+    #   @megaphone = MegaphoneClient.new({ network_id: '1234', organization_id: '5678', token: '910' })
+    #   @megaphone.episode #=> new MegaphoneClient::Podcast
 
     def podcast
       self::Podcast
